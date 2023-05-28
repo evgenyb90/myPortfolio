32d29ffa299120243a10e95058a071eca15b55c5
Evgeny <evgeny1bogdanov@gmail.com>
2023-05-28 13:44:24 +0300

    dark mode

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// dark mode check from localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem('darkMode') === 'light') {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+// dark mode
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle('dark');
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'light');
+    }
+}
